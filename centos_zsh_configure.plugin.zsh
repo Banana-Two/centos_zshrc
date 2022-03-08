7358d2aac4def4ab2b1116a0f55e125658330231
Banana-Two <1184903633@qq.com>
2022-03-09 06:25:35 +0800

    add new things and slove new problems

--- centos_zsh_configure.plugin.zsh
+++ centos_zsh_configure.plugin.zsh
@@ -24,10 +24,11 @@ alias pip='python3 -m pip'
 
 
 # Set Go language
+export GO111MODULE=on
 # Set the GOPROXY environment variable
 export GOPROXY=https://goproxy.io,direct
 # Set environment variable allow bypassing the proxy for specified repos (optional)
-export GOPRIVATE=git.mycompany.com,github.com/my/private
+# export GOPRIVATE=git.mycompany.com,github.com/my/private
 
 
 
@@ -78,6 +79,5 @@ MODE_INDICATOR="%F{white}<<<%f"
 
 #add local bin of normal user.
 export PATH=$PATH:$HOME/.local/bin
-
 
 
