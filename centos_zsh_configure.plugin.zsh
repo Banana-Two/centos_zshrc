02277fc5124a3149ad7fedfb63700e178a80c7df
Banana-Two <1184903633@qq.com>
2022-03-28 09:04:00 +0800

    change goneovim

--- centos_zsh_configure.plugin.zsh
+++ centos_zsh_configure.plugin.zsh
@@ -14,7 +14,7 @@ alias vi='vi -p'
 alias vim='vim -p'
 alias gvim='gvim -p'
 alias nvim='nvim -p'
-alias gnvim='~/Downloads/program/Goneovim-v0.5.1-linux/goneovim -p'
+alias gnvim='~/Downloads/program/goneovim-linux/goneovim -p'
 
 
 
