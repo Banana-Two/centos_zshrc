427b0c02ceed9f334964c1062109f76936d2728a
Banana-Two <1184903633@qq.com>
2022-07-14 20:32:56 +0800

    update go path

--- centos_zsh_configure.plugin.zsh
+++ centos_zsh_configure.plugin.zsh
@@ -24,15 +24,15 @@ alias pip='python3 -m pip'
 
 
 # Set Go language
-export GOPATH="$HOME/.go"
+export GOPATH="$HOME/.local/.go"
 export PATH="$PATH:${GOPATH//://bin:}/bin"
 export GO111MODULE=on
 # Set the GOPROXY environment variable
 export GOPROXY=https://goproxy.io,direct
 # Set environment variable allow bypassing the proxy for specified repos (optional)
 # export GOPRIVATE=git.mycompany.com,github.com/my/private
-if [ -d "/home-local/celab106_z2mini/.go/bin" ]; then
-  export PATH=$PATH:/home-local/celab106_z2mini/.go/bin
+if [ -d "/home-local/celab106_z2mini/.local/.go/bin" ]; then
+  export PATH=$PATH:/home-local/celab106_z2mini/.local/.go/bin
 fi
 
 
